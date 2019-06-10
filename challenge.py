c987bdc6286687d911f06c0a44173b8c295e5d10
Chris Kennedy <christopher.kennedy@verizon.com>
2019-06-10 14:32:03 -0400

    json parser

---
+++ challenge.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+dream_cars = {"car_1": "bugatti", "car_2": "ferarri"}
+boats = ["pinta", "Nina", "Santa-Maria"]
+
+print(f"The {(dream_cars['car_1']).capitalize()} was on the {boats[2]}, and the {dream_cars['car_2'].capitalize()} was on the {boats[0]}.")
+
---
+++ dict01/dict01.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+def main():
+    ## Create dict
+    switch = {'hostname': 'sw1', 'ip': '10.0.1.1', 'version': '1.2', 'vendor': 'cisco'}
+
+    ## Display
+    print( switch['hostname'] )
+    print( switch['ip'] )
+
+    ## request fake key
+    # print( switch['lynx'] )
+
+    ## request fake key with get
+    print( "First test - .get()" )
+    print (switch.get('lynx') )
+
+    print("Second test - .get()" )
+    print( switch.get('lynx', "THE KEY IS IN ANOTHER CASTLE!") )
+
+    print( "Third test - .get()")
+    print( switch.get('version') )
+
+    print( "Fourth test - .keys()" )
+    print( switch.keys() )
+
+    print("Fifth test - .values()" )
+    print(switch.values() )
+
+    print("Sixth test - .pop()" )
+    switch.pop('version') # Remove key/pair
+    print( switch.keys() )
+    print( switch.values() )
+
+    print("Seventh test - ADD new value" )
+    switch['adminlogin'] = 'karl08'
+    print( switch.keys() )
+    print( switch.values() )
+
+    print( "Eighth test - ADD a new value" )
+    switch['password'] = 'qwerty'
+    print( switch.keys() )
+    print( switch.values() )
+
+
+main()
